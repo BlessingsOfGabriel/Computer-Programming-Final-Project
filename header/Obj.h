63920e0b202766320aca54e9b95383c1af654f37
Gabriel <jimmy111923@gmail.com>
2020-12-22 13:45:58 +0800

    Add Obj.h

---
+++ header/Obj.h
@@ -0,0 +1,18 @@
+#ifndef OBJ_H
+#define OBJ_H
+class Obj{
+	public:
+		Obj();
+		~Obj();
+		int getHeight();
+		int getWidth();
+		void loadTexture(std::string);
+		virtual void resize(int newHeight, int new Width);
+		void free();
+	protected:
+		int _height, _width;
+		double _xPos, _yPos;
+		std::string _type;
+		SDL_Texture *_texture;
+};
+#endif
