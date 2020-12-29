24c0bd6cd2facc78535f587ea27d61fef845a412
Gabriel <jimmy111923@gmail.com>
2020-12-30 02:13:43 +0800

    Upload LoadMedia.h, Unit.h

---
+++ header/LoadMedia.h
@@ -0,0 +1,35 @@
+#ifndef LOADMEDIA_H
+#define LOADMEDIA_H
+
+class LoadedTexture {
+    private:
+        SDL_Texture* _loadTextureFromFile(std::string);
+        std::vector<std::string> _toLoadFileName;
+        std::map<std::string, SDL_Texture*> _loadedTextures;
+    public:
+        LoadedTexture();
+        ~LoadedTexture();
+        void loadAllTexture();
+        SDL_Texture* getTexture(std::string);
+        void free(); // Destroy Texture
+};
+
+class LoadedSound {
+    private:
+        Mix_Chunk* _loadSoundFromFile(std::string);
+        std::vector<std::string> _toLoadFileName;
+        std::map<std::string, Mix_Chunk*> _loadedSound;
+    public:
+        LoadedSound();
+        ~LoadedSound();
+        void loadAllSound();
+        Mix_Chunk* getSound(std::string);
+        void playSound(int, std::string, int);
+        void free();
+};
+
+
+extern LoadedTexture loadedTexture;
+extern LoadedSound loadedSound;
+
+#endif
---
+++ header/Unit.h
@@ -0,0 +1,8 @@
+#ifndef UNIT_H
+#define UNIT_H
+#include "Obj.h"
+class Unit{
+public:
+	
+};
+#endif
